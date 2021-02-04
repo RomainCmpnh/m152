7e2d42038d183875951a802a31c123a55019e619
RomainCmpnh <romain.cmpnh@eduge.ch>
2021-02-04 14:03:31 +0100

    bug correction chapter one

--- post.php
+++ post.php
@@ -17,8 +17,8 @@
             <div class="collapse navbar-collapse" id="navcol-1">
                 <div class="float-left float-md-right mt-5 mt-md-0 search-area"><i class="fas fa-search float-left search-icon"></i><input class="float-left float-sm-right custom-search-input" type="search" placeholder="Type to filter by address" style="width: 293px;margin-left: -4px;"></div>
                 <ul class="nav navbar-nav ml-auto">
-                    <li class="nav-item"><a class="nav-link active" href="index.html">HOME</a></li>
-                    <li class="nav-item"><a class="nav-link" href="post.html">POST</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="index.php">HOME</a></li>
+                    <li class="nav-item"><a class="nav-link" href="post.php">POST</a></li>
                     <li class="nav-item"></li>
                 </ul>
             </div>
